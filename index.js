21e9eec21fc009a4c87e53bd07cc88b0606e11bd
Casey Leask <caseyleask@gmail.com>
2014-01-11 09:43:10 +1100

    Using line-by-line streaming

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var fs = require("fs");
-var glob = require("glob")
-var test_files = glob.sync("data/*.dat");
+var fs = require("fs"),
+  glob = require("glob"),
+  LineByLineReader = require('line-by-line'),
+  test_files = glob.sync("data/*.dat");
 
 test_files.forEach(function(file) {
-  var stream = fs.createReadStream(file);
-  stream.setEncoding('utf8');
-  stream.on('data', function(data) {
-    console.log(file);
-    console.log(data);
+  var line_reader = new LineByLineReader(file);
+  line_reader.on('line', function(line) {
+    console.log(line);
   });
 });
