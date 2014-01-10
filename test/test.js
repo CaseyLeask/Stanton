6212fe950d6d5b7137fe560170a73eeea9b9e7a2
Casey Leask <caseyleask@gmail.com>
2014-01-10 15:47:13 +1100

    Added movement boundary checking tests

--- test/test.js
+++ test/test.js
@@ -35,4 +35,44 @@ describe("Stanton", function() {
 
     assert.equal("0,0,NORTH", Stanton.report(robot));
   });
+
+  it("Should move correctly", function() {
+    var robot = Stanton.place(0, 0, "north");
+
+    Stanton.move(robot);
+
+    assert.equal("0,1,NORTH", Stanton.report(robot));
+  });
+
+  it("Should not run over the north edge", function() {
+    var robot = Stanton.place(0, 4, "north");
+
+    Stanton.move(robot);
+
+    assert.equal("0,4,NORTH", Stanton.report(robot));
+  });
+
+  it("Should not run over the east edge", function() {
+    var robot = Stanton.place(4, 0, "east");
+
+    Stanton.move(robot);
+
+    assert.equal("4,0,EAST", Stanton.report(robot));
+  });
+
+  it("Should not run over the south edge", function() {
+    var robot = Stanton.place(0, 0, "south");
+
+    Stanton.move(robot);
+
+    assert.equal("0,0,SOUTH", Stanton.report(robot));
+  });
+
+  it("Should not run over the west edge", function() {
+    var robot = Stanton.place(0, 4, "west");
+
+    Stanton.move(robot);
+
+    assert.equal("0,4,WEST", Stanton.report(robot));
+  });
 });
