7d5809fd55a585b269301711ec23552c68d6ce70
Casey Leask <caseyleask@gmail.com>
2014-01-11 11:02:57 +1100

    Connected the library with the console.

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-var fs = require("fs"),
-  glob = require("glob"),
+var fs = require('fs'),
+  glob = require('glob'),
   LineByLineReader = require('line-by-line'),
-  test_files = glob.sync("data/*.dat");
+  test_files = glob.sync('data/*.dat'),
+  Transform = require('stream').Transform,
+  Stanton = require('./lib/stanton.js');
 
 test_files.forEach(function(file) {
-  var line_reader = new LineByLineReader(file);
+  var line_reader = new LineByLineReader(file),
+    ts = new Transform,
+    robot = Stanton.place(-1, -1, 'north');
+  
+  ts.setEncoding('utf8');
+  ts._transform = function(chunk, encoding, callback) {
+    var instructions = chunk.toString().split(/\s|,/);
+
+    switch(instructions[0]) {
+      case 'PLACE':
+        robot = Stanton.place(parseInt(instructions[1], 10),
+                              parseInt(instructions[2], 10),
+                              instructions[3].toLowerCase());
+        break;
+      case 'MOVE':
+        Stanton.move(robot);
+        break;
+      case 'LEFT':
+        Stanton.left(robot);
+        break;
+      case 'RIGHT':
+        Stanton.right(robot);
+        break;
+      case 'REPORT':
+        ts.push(Stanton.report(robot));
+        break;
+    }
+    callback();
+  };
+
   line_reader.on('line', function(line) {
-    console.log(line);
+    ts.write(line);
+  });
+
+  line_reader.on('end', function(line) {
+    ts.end();
+    ts.pipe(process.stdout);
   });
 });
